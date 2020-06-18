23b88e825232d135cb17ad32fa895bcd83d3cb34
garritfra <garritfranke@gmail.com>
2020-06-18 20:11:36 +0200

    feat: match string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+extern crate regex;
+use regex::Regex;
+
 fn main() {
-    let tokens = tokenize("Hello World".to_string()).unwrap();
+    let tokens = tokenize("hello 'World'".to_string()).unwrap();
     println!("{:?}", tokens);
 }
 
@@ -15,12 +18,21 @@ fn tokenize(string: String) -> Result<Vec<Token>, String> {
     let mut index = 0;
 
     'mainloop: while index < string.len() {
-        let token = Token {
-            name: "Foo".to_string(),
-            raw: string.chars().nth(index).unwrap().to_string(),
-        };
-        tokens.push(token);
+        let rest: String = string.chars().skip(index).collect();
+
+        println!("Rest: {:?}", rest);
+        if let Some(m) = Regex::new(r#"^'([^\\]|\\.)*?'|^"([^\\]|\\.)*?"#)
+            .unwrap()
+            .captures(&rest)
+        {
+            let token = Token {
+                name: "string".to_string(),
+                raw: m.get(0).unwrap().as_str().to_string(),
+            };
+            tokens.push(token);
+        }
         index += 1;
+        continue 'mainloop;
     }
 
     Ok(tokens)
