a85894332139fd02948068aebd647c3f427f8643
garritfra <garritfranke@gmail.com>
2020-06-18 19:08:45 +0200

    Add simple tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let tokens = tokenize("Hello World".to_string()).unwrap();
+    println!("{:?}", tokens);
+}
+
+#[derive(Debug)]
+struct Token {
+    name: String,
+    raw: String,
+}
+
+fn tokenize(string: String) -> Result<Vec<Token>, String> {
+    let mut tokens = Vec::new();
+
+    let mut index = 0;
+
+    'mainloop: while index < string.len() {
+        let token = Token {
+            name: "Foo".to_string(),
+            raw: string.chars().nth(index).unwrap().to_string(),
+        };
+        tokens.push(token);
+        index += 1;
+    }
+
+    Ok(tokens)
 }
