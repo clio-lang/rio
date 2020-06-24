c5fde0b4de7c143ba1ea127122764e4377e92a2b
garritfra <garrit.franke@ckc.de>
2020-06-24 21:34:36 +0200

    feat: proper lexer barebones

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,104 @@
 extern crate regex;
-use regex::Regex;
+use crate::TokenKind::Unknown;
+use crate::TokenKind::Whitespace;
+
+mod cursor;
+use cursor::Cursor;
 
 fn main() {
-    let tokens = tokenize("hello 'World' 1 23 345".to_string()).unwrap();
+    let tokens = tokenize(&"1 12 123");
 
     for token in tokens {
-        println!("{:?}", token);
+        println!("{:?}", token)
     }
 }
 
 #[derive(Debug)]
-struct Token {
-    name: String,
-    raw: String,
+pub struct Token {
+    pub kind: TokenKind,
+    pub len: usize,
 }
 
-fn tokenize(string: String) -> Result<Vec<Token>, String> {
-    let mut tokens = Vec::new();
+impl Token {
+    fn new(kind: TokenKind, len: usize) -> Token {
+        Token { kind, len }
+    }
+}
 
-    let mut index = 0;
+/// Enum representing common lexeme types.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum TokenKind {
+    /// Any whitespace characters sequence.
+    Whitespace,
+    Literal {
+        kind: LiteralKind,
+        suffix_start: usize,
+    },
 
-    'mainloop: while index < string.len() {
-        let rest: String = string.chars().skip(index).collect();
+    /// Unknown token, not expected by the lexer, e.g. "№"
+    Unknown,
+}
 
-        // TODO: Use pattern matching instead
-        if let Some(m) = Regex::new(r#"^'([^\\]|\\.)*?'|^"([^\\]|\\.)*?"#)
-            .unwrap()
-            .captures(&rest)
-        {
-            let token = Token {
-                name: "string".to_string(),
-                raw: m.get(0).unwrap().as_str().to_string(),
-            };
-            index += token.raw.len();
-            println!("{:?}", token.raw.len());
-            tokens.push(token);
-        } else if let Some(m) = Regex::new(r#"^(0|-?[1-9][0-9']*)(n|(\.[0-9']+))?"#)
-            .unwrap()
-            .captures(&rest)
-        {
-            let token = Token {
-                name: "number".to_string(),
-                raw: m.get(0).unwrap().as_str().to_string(),
-            };
-            index += token.raw.len();
-            tokens.push(token);
-        } else {
-            index += 1;
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum LiteralKind {
+    /// "12_u8", "0o100", "0b120i99"
+    Int { empty_int: bool },
+}
+
+/// Creates an iterator that produces tokens from the input string.
+pub fn tokenize(mut input: &str) -> impl Iterator<Item = Token> + '_ {
+    std::iter::from_fn(move || {
+        if input.is_empty() {
+            return None;
         }
-        continue 'mainloop;
+        let token = first_token(input);
+        input = &input[token.len..];
+        Some(token)
+    })
+}
+
+/// Parses the first token from the provided input string.
+pub fn first_token(input: &str) -> Token {
+    debug_assert!(!input.is_empty());
+    Cursor::new(input).advance_token()
+}
+
+pub fn is_whitespace(c: char) -> bool {
+    match c {
+        '\t' | '\n' | 'r' | ' ' => true,
+        _ => false,
+    }
+}
+
+impl Cursor<'_> {
+    /// Parses a token from the input string.
+    fn advance_token(&mut self) -> Token {
+        let first_char = self.bump().unwrap();
+        let token_kind = match first_char {
+            c if is_whitespace(c) => self.whitespace(),
+            _ => Unknown,
+        };
+        Token::new(token_kind, self.len_consumed())
     }
 
-    Ok(tokens)
+    /// Eats symbols while predicate returns true or until the end of file is reached.
+    /// Returns amount of eaten symbols.
+    fn eat_while<F>(&mut self, mut predicate: F) -> usize
+    where
+        F: FnMut(char) -> bool,
+    {
+        let mut eaten: usize = 0;
+        while predicate(self.first()) && !self.is_eof() {
+            eaten += 1;
+            self.bump();
+        }
+
+        eaten
+    }
+
+    fn whitespace(&mut self) -> TokenKind {
+        debug_assert!(is_whitespace(self.prev()));
+        self.eat_while(is_whitespace);
+        Whitespace
+    }
 }
